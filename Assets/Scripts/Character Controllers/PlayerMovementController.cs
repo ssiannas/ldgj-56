802021417136e1593018daad152a94051621c0cd
SpyrosSiannas <ssiannas@gmail.com>
2024-10-05 23:16:34 +0300

    Add idle animation

--- Assets/Scripts/Character Controllers/PlayerMovementController.cs
+++ Assets/Scripts/Character Controllers/PlayerMovementController.cs
@@ -8,18 +8,32 @@ public class PlayerMovementController
 	private readonly Rigidbody2D _rb;
 	private Vector2 _direction;
 	private readonly PlayerController _playerController;
+	private readonly Animator _animator;
 
 	public PlayerMovementController(PlayerController playerController)
 	{
 		_playerController = playerController;
 		_rb = _playerController.GetComponent<Rigidbody2D>();
+		_animator = _playerController.GetComponent<Animator>();
 	}
 
 
 	public void OnFixedUpdate()
 	{
 		var speed = _playerController.speed;
+		MaybeWalk();
 		_rb.MovePosition(_rb.position + _direction * speed * Time.fixedDeltaTime);
+	}
+
+	private void MaybeWalk()
+	{
+		if (_direction != Vector2.zero)
+		{
+			_animator.SetBool("isWalking", true);
+		} else
+		{
+			_animator.SetBool("isWalking", false);
+		}
 	}
 
     public void OnUpdate()
@@ -31,6 +45,11 @@ public class PlayerMovementController
 
 	private void MaybeFlipSprite()
 	{
+		if (_direction == Vector2.zero)
+		{
+			return;
+		}
+
 		_playerController.GetComponent<SpriteRenderer>().flipX = _direction.x > 0;	
 	}	
 }
