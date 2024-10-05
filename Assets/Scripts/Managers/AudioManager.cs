f06cb980b79f83b263c45e871a5a4254964b6cbb
George Tzoupis <giwrgos0gt15@gmail.com>
2024-10-05 11:49:22 +0300

    Added singleton managers for Game, Audio, Menu

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            // If there is an instance, and it's not me, delete myself.
+
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            // If there is an instance, and it's not me, delete myself.
+
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    }
+}
