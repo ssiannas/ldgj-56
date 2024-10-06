6759ecc538773244d2b03f83e6dee58741455b29
SpyrosSiannas <ssiannas@gmail.com>
2024-10-06 15:29:12 +0300

    Add better camera init

--- Assets/Scripts/Game Controllers/Camera Controller.cs
+++ Assets/Scripts/Game Controllers/Camera Controller.cs
@@ -9,16 +9,34 @@ public class SmoothCamera2D : MonoBehaviour
 	private Vector3 velocity = Vector3.zero;
 	public GameObject target;
 
+	private void Start()
+	{
+		target = GameObject.FindGameObjectWithTag("Player");
+		FollowTqarget(target, false);	
+	}
+
 	// Update is called once per frame
 	void FixedUpdate()
 	{
 		if (target)
 		{
-			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
-			Vector3 delta = target.transform.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			FollowTqarget(target);
 		}
 
+	}
+
+	private void FollowTqarget(GameObject target, bool smooth = true)
+	{
+		Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
+		Vector3 delta = target.transform.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+		Vector3 destination = transform.position + delta;
+		if (smooth)
+		{
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+		else
+		{
+			transform.position = destination;
+		}
 	}
 }
