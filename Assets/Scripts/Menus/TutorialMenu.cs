cb9942ca7e53ae695b58ce72ed869bd2326b7f1b
George Tzoupis <giwrgos0gt15@gmail.com>
2024-10-07 15:06:21 +0300

    Added Tutoriel

--- Assets/Scripts/Menus/TutorialMenu.cs
+++ Assets/Scripts/Menus/TutorialMenu.cs
@@ -1,18 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 
 public class TutorialMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject ObjectiveSlide;
+    [SerializeField] private GameObject PlayerSlide;
+    [SerializeField] private GameObject EnemiesSlide;
+
+    enum State
     {
-        
+        Objective,
+        Player,
+        Enemies
     }
 
-    // Update is called once per frame
-    void Update()
+    private State state;
+
+    private void OnEnable()
     {
-        
+        state = State.Objective;
+        HideAll();
+        ObjectiveSlide.SetActive(true);
+    }
+
+    private void HideAll()
+    {
+        ObjectiveSlide.SetActive(false);
+        PlayerSlide.SetActive(false);
+        EnemiesSlide.SetActive(false);
+    }
+
+    public void PreviousClickHandle()
+    {
+        switch (state)
+        {
+            case State.Objective:
+                state = State.Objective;
+                UIManager.Instance.ShowStartMenu();
+                break;
+            case State.Player:
+                state = State.Objective;
+                HideAll();
+                ObjectiveSlide.SetActive(true);
+                break;
+            case State.Enemies:
+                state = State.Player;
+                HideAll();
+                PlayerSlide.SetActive(true);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void NextClickHandle()
+    {
+        switch (state)
+        {
+            case State.Objective:
+                state = State.Player;
+                HideAll();
+                PlayerSlide.SetActive(true);
+                break;
+            case State.Player:
+                state = State.Enemies;
+                HideAll();
+                EnemiesSlide.SetActive(true);
+                break;
+            case State.Enemies:
+                state = State.Objective;
+                HideAll();
+                UIManager.Instance.ShowStartMenu();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
