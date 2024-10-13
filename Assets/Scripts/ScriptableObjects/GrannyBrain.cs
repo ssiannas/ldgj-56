ca3d79dff4a4b0363b8e099e61ed8804ac7a993d
George Tzoupis <giwrgos0gt15@gmail.com>
2024-10-13 21:59:04 +0300

    Added fall animation for nerd kit

--- Assets/Scripts/ScriptableObjects/GrannyBrain.cs
+++ Assets/Scripts/ScriptableObjects/GrannyBrain.cs
@@ -6,132 +6,137 @@ using UnityEngine.Rendering.UI;
 using static Unity.VisualScripting.Member;
 
 [CreateAssetMenu(fileName = "GrannyBrain", menuName = "ScriptableObjects/EnemyAI/GrannyBrain", order = 1)]
-
 public class GrannyBrain : EnemyBrain
 {
-	[SerializeField] public LayerMask obstacleLayer;
-	[SerializeField] private float moveSpeed = 4f;
+    [SerializeField] public LayerMask obstacleLayer;
+    [SerializeField] private float moveSpeed = 4f;
 
-	private Vector2 lastKnownPosition = Vector2.zero;
-	private static Int32 COLLISIONS_LAYER_MASK = 1 << 3;
-	[SerializeField] private float grannyEyesightRange = 1.5f;
+    private Vector2 lastKnownPosition = Vector2.zero;
+    private static Int32 COLLISIONS_LAYER_MASK = 1 << 3;
+    [SerializeField] private float grannyEyesightRange = 1.5f;
 
-	[SerializeField] private AudioChannel _audioChannel;
-	
-	private System.Random _random = new System.Random();
-	[SerializeField] private GameObject kidFaintPrefab;
-	private int tauntCountToFaint = 5;
+    [SerializeField] private AudioChannel _audioChannel;
 
-	public void OnEnable()
-	{
-		obstacleLayer = COLLISIONS_LAYER_MASK;
-	}
+    private System.Random _random = new System.Random();
+    [SerializeField] private GameObject kidFaintPrefab;
+    private int tauntCountToFaint = 5;
 
-	public override float GetEyesightRange()
-	{
-		return grannyEyesightRange;
-	}
+    public void OnEnable()
+    {
+        obstacleLayer = COLLISIONS_LAYER_MASK;
+    }
 
-	public Sound GetRandomScream() { 
-		return enemySounds[_random.Next(0, enemySounds.Count - 2)];
-	}
-		 
-	public override void Think(EnemyController entity)
-	{
-		if (entity.playerInRange)
-		{
-			ShootRayTowardsPlayer(entity);
-		}
-		
-		if (lastKnownPosition != Vector2.zero)
-		{
-			Debug.DrawLine(entity.transform.position, lastKnownPosition, Color.red);
-		}
+    public override float GetEyesightRange()
+    {
+        return grannyEyesightRange;
+    }
 
-		switch (entity.state)
-		{
-			case EnemyController.State.IDLE:
-				break;
-			case EnemyController.State.ALERT:
-				break;
-			case EnemyController.State.FLEEING:
-				MoveAwayFromPlayer(entity);
-				break;
-			default:
-				break;
-		}
-	}
+    public Sound GetRandomScream()
+    {
+        return enemySounds[_random.Next(0, enemySounds.Count - 2)];
+    }
 
-	private void ShootRayTowardsPlayer(EnemyController entity)
-	{
-		Transform entityTransform = entity.transform;
-		if (entity.playerTransform != null)
-		{
-			Vector2 directionToPlayer = (entity.playerTransform.position - entityTransform.position).normalized;
-			LayerMask mask = obstacleLayer | GetPlayerLayer();
-			RaycastHit2D hit = Physics2D.Raycast(entityTransform.position, directionToPlayer, GetEyesightRange(), mask);
+    public override void Think(EnemyController entity)
+    {
+        if (entity.playerInRange)
+        {
+            ShootRayTowardsPlayer(entity);
+        }
 
-			if (hit.collider != null && hit.collider.CompareTag("Player"))
-			{
-				lastKnownPosition = entity.playerTransform.position;
-				MoveStateToFleeing(entity);
-			}
-		}
-	} 
-	
-	private void MoveStateToFleeing(EnemyController entity)
-	{
-		if (entity.state == EnemyController.State.FLEEING) return;
-		entity.state = EnemyController.State.FLEEING;
-		entity.animator?.SetBool("isFleeing", true);
-		entity.PlayScream();
-	}
+        if (lastKnownPosition != Vector2.zero)
+        {
+            Debug.DrawLine(entity.transform.position, lastKnownPosition, Color.red);
+        }
 
+        switch (entity.state)
+        {
+            case EnemyController.State.IDLE:
+                break;
+            case EnemyController.State.ALERT:
+                break;
+            case EnemyController.State.FLEEING:
+                MoveAwayFromPlayer(entity);
+                break;
+            default:
+                break;
+        }
+    }
 
+    private void ShootRayTowardsPlayer(EnemyController entity)
+    {
+        Transform entityTransform = entity.transform;
+        if (entity.playerTransform != null)
+        {
+            Vector2 directionToPlayer = (entity.playerTransform.position - entityTransform.position).normalized;
+            LayerMask mask = obstacleLayer | GetPlayerLayer();
+            RaycastHit2D hit = Physics2D.Raycast(entityTransform.position, directionToPlayer, GetEyesightRange(), mask);
 
-	private void MoveAwayFromPlayer(EnemyController entity)
-	{
-		Transform entityTransform = entity.transform;
-		Vector2 directionToTarget = (lastKnownPosition - (Vector2)entityTransform.position).normalized;
-		entity.Move(-directionToTarget, moveSpeed);
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+            {
+                lastKnownPosition = entity.playerTransform.position;
+                MoveStateToFleeing(entity);
+            }
+        }
+    }
+
+    private void MoveStateToFleeing(EnemyController entity)
+    {
+        if (entity.state == EnemyController.State.FLEEING) return;
+        entity.state = EnemyController.State.FLEEING;
+        entity.animator?.SetBool("isFleeing", true);
+        entity.PlayScream();
+    }
 
 
-		if (Vector2.Distance(entityTransform.position, lastKnownPosition) > 2*GetEyesightRange()) // Adjust the threshold as needed
-		{
-			StopFleeing(entity);
-			// Maybe implement patrol system with range around LKP
-		}
-	}
+    private void MoveAwayFromPlayer(EnemyController entity)
+    {
+        Transform entityTransform = entity.transform;
+        Vector2 directionToTarget = (lastKnownPosition - (Vector2)entityTransform.position).normalized;
+        entity.Move(-directionToTarget, moveSpeed);
 
-	private void StopFleeing(EnemyController entity)
-	{
-		entity.state = EnemyController.State.IDLE;
-		entity.animator?.SetBool("isFleeing", false);
-	}
 
-	public override void OnTaunt(EnemyController entity, int tauntCount)
-	{
-		if (tauntCount == tauntCountToFaint)
-		{
-			Vector3 pos = entity.transform.position;
-			entity.PlayAudio(enemySounds.Find(s => s.soundName == "Faint"));
-			var kid = Instantiate(kidFaintPrefab, pos, Quaternion.Euler(0, 0, 90));
-			PlayFaintOnKid(kid);
-			entity.StopCoroutine(entity.ShowReaction());
-			Destroy(entity.gameObject);
-		}
-	}
+        if (Vector2.Distance(entityTransform.position, lastKnownPosition) >
+            2 * GetEyesightRange()) // Adjust the threshold as needed
+        {
+            StopFleeing(entity);
+            // Maybe implement patrol system with range around LKP
+        }
+    }
 
-	public void PlayFaintOnKid(GameObject kid)
-	{
-		var source = kid.AddComponent<AudioSource>();
-		var s = enemySounds.Find(s => s.soundName == "Faint");
-		if (s is null) return;
-		source.clip = s.clip;
-		source.volume = s.volume;
-		source.pitch = s.pitch;
-		source.loop = s.loop;
-		s.source = source;
-		source.Play();
-	}
+    private void StopFleeing(EnemyController entity)
+    {
+        entity.state = EnemyController.State.IDLE;
+        entity.animator?.SetBool("isFleeing", false);
+    }
+
+    public override void OnTaunt(EnemyController entity, int tauntCount)
+    {
+        if (tauntCount == tauntCountToFaint)
+        {
+            Vector3 pos = entity.transform.position;
+            entity.PlayAudio(enemySounds.Find(s => s.soundName == "Faint"));
+            var kid = Instantiate(kidFaintPrefab, pos, Quaternion.identity);
+            var kidScale = kid.transform.localScale;
+            kidScale.x = entity.transform.localScale.x < 0
+                ? -Mathf.Abs(kid.transform.localScale.x)
+                : Mathf.Abs(kid.transform.localScale.x);
+            kid.transform.localScale = kidScale;
+            PlayFaintOnKid(kid);
+            entity.StopCoroutine(entity.ShowReaction());
+            Destroy(entity.gameObject);
+        }
+    }
+
+    public void PlayFaintOnKid(GameObject kid)
+    {
+        var source = kid.AddComponent<AudioSource>();
+        var s = enemySounds.Find(s => s.soundName == "Faint");
+        if (s is null) return;
+        source.clip = s.clip;
+        source.volume = s.volume;
+        source.pitch = s.pitch;
+        source.loop = s.loop;
+        s.source = source;
+        source.Play();
+    }
 }
